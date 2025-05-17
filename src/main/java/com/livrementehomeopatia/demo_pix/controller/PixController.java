b8b126261dcaf0333f4661c82825cd93f54bc7fd
sergioizaiasparreiras <sergioparreiras09@gmail.com>
2025-05-17 14:16:49 -0300

    feat: controller pix

--- src/main/java/com/livrementehomeopatia/demo_pix/controller/PixController.java
+++ src/main/java/com/livrementehomeopatia/demo_pix/controller/PixController.java
@@ -3,6 +3,7 @@ package com.livrementehomeopatia.demo_pix.controller;
 
 import com.livrementehomeopatia.demo_pix.service.PixService;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/v1/pix", produces = MediaType.APPLICATION_JSON_VALUE)
 public record PixController(PixService pixService){
 
-    public ResponseEntity<String> listPixKeys()
+    @RequestMapping("/listar")
+    public ResponseEntity<String> listPixKeys(){
+        var response = this.listPixKeys();
+        return ResponseEntity.ok().body(response.toString());
+    }
 }
